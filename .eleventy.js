44b7e62104fafda62aeb6605ac5bf60f23748870
krhmwn <krhmwn@users.noreply.github.com>
2025-10-25 23:04:55 +0700

    251025 7

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight'),
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPlugin(syntaxHighlight);
   eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPassthroughCopy('favicon.ico');
+  eleventyConfig.addPassthroughCopy('assets/logo');
   eleventyConfig.addPassthroughCopy('assets/fonts');
   const options = {
     html: true,
